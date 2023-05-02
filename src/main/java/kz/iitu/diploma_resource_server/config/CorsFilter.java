af2d2e56c20775f7d8c4aadb890b71d8d7aec3f9
Yerkebulan Baikonyssov <klok_good@mail.ru>
2023-05-02 22:28:30 +0600

    fix: cors policy

--- src/main/java/kz/iitu/diploma_resource_server/config/CorsFilter.java
+++ src/main/java/kz/iitu/diploma_resource_server/config/CorsFilter.java
@@ -28,12 +28,10 @@ public class CorsFilter implements Filter {
         HttpServletResponse response = (HttpServletResponse) resp;
         HttpServletRequest request = (HttpServletRequest) req;
         response.setHeader("Access-Control-Allow-Origin", "*");
+        response.setHeader("Access-Control-Allow-Methods", "*");
+        response.setHeader("Access-Control-Allow-Headers", "*");
         response.setHeader("Access-Control-Allow-Credentials", "true");
-        response.setHeader("Access-Control-Allow-Methods", "POST, GET, OPTIONS, DELETE, PUT");
         response.setHeader("Access-Control-Max-Age", "3600");
-        response
-                .setHeader("Access-Control-Allow-Headers", "Origin, origin, x-requested-with, authorization, " +
-                        "Content-Type, Authorization, credential, X-XSRF-TOKEN");
 
         if ("OPTIONS".equalsIgnoreCase(request.getMethod())) {
             response.setStatus(HttpServletResponse.SC_OK);
